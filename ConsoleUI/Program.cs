09741cdbf47678c1538dfcdfd31e84769f8da44d
Melih Sivri <mlheymen.ms@gmail.com>
2021-02-27 20:04:30 +0300

    ders 9

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -12,8 +12,18 @@ namespace ConsoleUI
     {
         static void Main(string[] args)
         {
+            //ProductTest();
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (Category category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
-            foreach (Product product in productManager.GetByUnitPrice(1,10))
+            foreach (Product product in productManager.GetByUnitPrice(1, 10))
             {
                 Console.WriteLine(product.ProductName);
             }
